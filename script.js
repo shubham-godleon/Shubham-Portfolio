f763abed4ab951fc6677977e445d288c0be79c52
Shubham Kumar <shubhamhppaviliong6@gmail.com>
2024-01-05 01:31:35 +0530

    Finally half done

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const icon = document.querySelector(".hamburger-icon");
+    const menu = document.querySelector(".menu-links");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
